e47633ec6670061e4e2986807897eade46706974
Anika Sood <soodanika2000@gmail.com>
2026-02-19 13:12:54 +0530

    Day1 - Variables

---
+++ Week-1/Day1/Practice.java
@@ -0,0 +1,22 @@
+class Practice {
+    public static void main(String arg[]){
+        int age = 26;
+        double salary = 1200000.456;
+        char grade = 'A';
+        boolean isEmployed = true;
+        String name = "Anika";
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isEmployed);
+        System.out.println(name);
+
+        int a = 20;
+        int b = 4;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
---
+++ Week-1/Day1/Variables.java
@@ -0,0 +1,8 @@
+class Variables {
+    public static void main (String arg[]){
+        int num1 = 1;
+        int num2 = 34;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
