02fffe815fb0588bda75a9dfa2bac92d0e386d9a
Anika Sood <soodanika2000@gmail.com>
2026-02-20 11:18:22 +0530

    If Else

---
+++ Week-1/Day2/Practice.java
@@ -0,0 +1,33 @@
+class Practice {
+    public static void main (String args[]){
+
+        // EVEN_ODD
+//        int num = 23;
+//        if(num%2==0)
+//            System.out.println(num + "is even");
+//        else
+//            System.out.println(num+" is odd");
+
+        // Positive Negative Zero
+//        int num = 0;
+//        if (num<0)
+//            System.out.println("Number is negative");
+//        else if (num>0)
+//            System.out.println("Number is positive");
+//        else
+//            System.out.println("Number is zero");
+
+            // Maximum of 3
+
+        int a = 2;
+        int b = 3;
+        int c = 7;
+
+        if (a>b && a>c)
+            System.out.println("A is greatest");
+        else if (b>a && b>c)
+            System.out.println("B is greatest");
+        else
+            System.out.println("C is greatest");
+    }
+}
